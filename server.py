85eeb8e958c606ac834d13471c8954440a742c01
Kennouille <muhlemannk@gmail.com>
2025-06-09 11:32:17 -0600

    fix erreur

--- server.py
+++ server.py
@@ -2254,9 +2254,10 @@ def apply_selected_tags(merged_db_path, db1_path, db2_path, note_choices, note_m
                     if not old_note_id:
                         continue
 
+                    print(f"📌 Contenu initial de note_mapping: {note_mapping}")
+
                     source_db = db1_path if source == "file1" else db2_path
                     new_note_id = note_mapping.get((source_db, old_note_id))
-                    print(f"🔍 Mapping vérifié: old_note_id={old_note_id}, source_db={source_db}, new_note_id={new_note_id}")  # Ajout ici
 
                     if not new_note_id:
                         continue
